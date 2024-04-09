ddeab21db623b6c56e44ce2e71a4d4cb5722c3ab
AmirastanM <amirastanshm@code.edu.az>
2024-04-09 18:02:30 +0400

    added new mini project

---
+++ Repository/AppDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Group = Domain.Models.Group;
+
+namespace Repository
+{
+    internal class AppDbContext : DbContext
+    {
+
+        public DbSet<Education> Educations { get; set; }
+        public DbSet<Group> Groups { get; set; }
+        public DbSet<User> Users { get; set; }
+
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=amirastan-pc\\sqlexpress;Initial Catalog=EF_CourseApp;Integrated Security=True");
+
+        }
+    }
+
+}
