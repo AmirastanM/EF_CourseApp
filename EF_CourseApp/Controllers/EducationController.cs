88769b3e441eca1b10b498fc7ec7597494c334e7
AmirastanM <amirastanshm@code.edu.az>
2024-04-12 12:48:11 +0400

    complete relations between layers and ended education getall method

---
+++ EF_CourseApp/Controllers/EducationController.cs
@@ -0,0 +1,35 @@
+﻿using Service.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace EF_CourseApp.Controllers
+{
+    internal class EducationController
+    {
+        private readonly EducationService _educationService;
+
+        public EducationController()
+        {
+            _educationService = new EducationService();
+        }
+
+        public async Task  GetAllAsync()
+        {
+            var datas = await _educationService.GetAllAsync();
+            if (datas != null || !datas.Any()) 
+            {
+                await Console.Out.WriteLineAsync("Data not found");
+            }
+            foreach (var item in datas) 
+            {
+                string data = $"Education Name: {item.Name}, Education Color: {item.Color}";
+                await Console.Out.WriteLineAsync(data);
+            }
+        }
+
+    }
+}
